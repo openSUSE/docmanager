f3865db119639c46678b6ab4cf3bd39ee61a7ae9
Thomas Schraitle <tom_schr@web.de>
2015-03-18 09:59:47 +0100

    Prepared test environment for DocManager
    * Used ideas from
      - https://github.com/pypa/sampleproject
      - http://pytest.org/latest/goodpractises.html
    * Created main script in __init__.py/main()
    * Created setup.py script and requirements.txt for pip

--- docmanager/table.py
+++ docmanager/table.py
@@ -17,7 +17,7 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
-import prettytable
+from docmanager import prettytable
 
 class Table:
 
