250f6afa9df3f547cd71372c7f894c2cc83e0dee
Manuel Schnitzer <mschnitzer@suse.de>
2015-05-29 11:08:22 +0200

    Fixed entity issue

--- docmanager/xmlhandler.py
+++ docmanager/xmlhandler.py
@@ -16,10 +16,12 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
+import random
 import re
 import sys
 from docmanager.core import DefaultDocManagerProperties, NS, ReturnCodes
 from docmanager.logmanager import log, logmgr_flog
+from docmanager.tmpfile import TmpFile
 from lxml import etree
 
 def localname(tag):
@@ -67,6 +69,14 @@ class XmlHandler(object):
                    'synopsis', 'table', 'task', 'taskprerequisites', 'taskrelated',
                    'tasksummary', 'textdata', 'textobject', 'tip', 'toc', 'tocdiv',
                    'topic', 'variablelist', 'videodata', 'videoobject', 'warning')
+    
+    orig_filename = ""
+    
+    tmp_orig = None
+    tmp_updated = None
+    
+    entity_replacements = {}
+    entity_include_replacements = {}
 
     def __init__(self, filename):
         """Initializes the XmlHandler class
@@ -74,18 +84,93 @@ class XmlHandler(object):
         :param str filename: filename of XML file
         """
         logmgr_flog()
+        
+        self.orig_filename = filename
+        
+        # save the content into a tmp file
+        with open(filename, 'r') as f:
+            content = f.read()
+            
+            self.tmp_orig = TmpFile()
+            self.tmp_orig.write(content)
+            
+            log.debug("XmlHandler: Stored original content of {} in tmp file: {}".format(filename, self.tmp_orig.filename))
+            
+            self.tmp_updated = TmpFile()
+            self.tmp_updated.write(self.replace_entities(self.replace_entity_includes(content)))
+            
+            log.debug("XmlHandler: Stored modified content of {} in tmp file: {}".format(filename, self.tmp_updated.filename))
+
         #register the namespace
         etree.register_namespace("dm", "{dm}".format(**self.__namespace))
         self.__xmlparser = etree.XMLParser(remove_blank_text=False,
                                            resolve_entities=False,
                                            dtd_validation=False)
         #load the file and set a reference to the dm group
-        self.__tree = etree.parse(filename, self.__xmlparser)
+        self.__tree = etree.parse(self.tmp_updated.filename, self.__xmlparser)
         self.__root = self.__tree.getroot()
         self.__docmanager = self.__tree.find("//dm:docmanager",
                                              namespaces=self.__namespace)
         if self.__docmanager is None:
             self.create_group()
+
+    def replace_entity_includes(self, content):
+        r = re.findall('%[a-zA-Z0-9\-_\.]+;', content)
+        if r is not None:
+            for i in r:
+                rstr = self.gen_entity_identifier()
+
+                self.entity_include_replacements["<!-- {} -->".format(i)] = i
+                content = content.replace(i, "<!-- {} -->".format(i))
+
+        return content
+
+    def replace_entities(self, content):
+        r = re.findall('&[a-zA-Z0-9\-_\.]+;', content)
+        if r is not None:
+            for i in r:
+                rstr = self.gen_entity_identifier()
+
+                self.entity_replacements["{}".format(rstr)] = i
+                content = content.replace(i, "{}".format(rstr))
+
+        return content
+    
+    def gen_entity_identifier(self):
+        string = ""
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+        i = 1
+        while i <= 16:
+            string += chars[random.randrange(0,len(chars))]
+            i += 1
+
+        return "{}".format(string)
+    
+    def convert_back(self, filename):
+        content = ""
+        
+        # convert the entities and the entity includes back
+        with open(filename, 'r') as f:
+            content = f.read()
+            if len(self.entity_replacements):
+                for key, value in self.entity_replacements.items():
+                    content = content.replace(key, value)
+            
+            if len(self.entity_include_replacements):
+                for key, value in self.entity_include_replacements.items():
+                    content = content.replace(key, value)
+            
+            # try to indent entity includes... (That is needed since lxml destroys the indentation in the DOCTYPE)
+            r = re.findall('%[a-zA-Z0-9\-_\.]+;\]>', content)
+            if r is not None:
+                for i in r:
+                    tmp = i.split("]")
+                    content = content.replace(i, "{}\n]{}".format(tmp[0], tmp[1]))
+        
+        # try to indent entity includes... (That is needed since lxml destroys the indentation in the DOCTYPE)
+        with open(filename, 'w') as f:
+            f.write(content)
 
     def init_default_props(self, force):
         ret = 0
@@ -279,9 +364,19 @@ class XmlHandler(object):
         logmgr_flog()
         # Only indent docmanager child elements
         self.indent_dm()
-        self.__tree.write(self.filename,
+        self.__tree.write(self.tmp_orig.filename,
                           # pretty_print=True,
                           with_tail=True)
+        
+        self.convert_back(self.tmp_orig.filename)
+        
+        content = ""
+        with open(self.tmp_orig.filename, 'r') as f:
+            content = f.read()
+        
+        if len(content) > 0:
+            with open(self.orig_filename, 'w') as f:
+                f.write(content)
 
     @property
     def filename(self):
