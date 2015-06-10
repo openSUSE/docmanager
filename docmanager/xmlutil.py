9cdd2ec68262b75a7c24f5148e999cff6005084e
Thomas Schraitle <tom_schr@web.de>
2015-06-10 14:03:37 +0200

    Fixed pylint unused-import (W0611)

--- docmanager/xmlutil.py
+++ docmanager/xmlutil.py
@@ -16,7 +16,6 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
-import copy
 from collections import namedtuple
 from docmanager.core import ReturnCodes
 from io import StringIO
