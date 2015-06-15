b5a1b91dae340aaea69fe87dc82ba239381a077c
Manuel Schnitzer <mschnitzer@suse.de>
2015-06-15 16:05:59 +0200

    Prepared release for RC4

--- docmanager/__init__.py
+++ docmanager/__init__.py
@@ -17,7 +17,7 @@
 # you may find current contact information at www.suse.com
 
 __author__="Rick Salevsky, Manuel Schnitzer, and Thomas Schraitle"
-__version__="3.0.0-RC3"
+__version__="3.0.0-RC4"
 
 from docmanager.action import Actions
 from docmanager.cli import parsecli
