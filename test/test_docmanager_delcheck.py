498264f5036060f96addd0a728d18e84a6ea80ba
Manuel Schnitzer <mschnitzer@suse.de>
2015-07-27 12:44:05 +0200

    Add new test case and fix existing ones
    * Add test case for #60
    * Fix: Add 'write' method - otherwise the tests won't work

--- test/test_docmanager_delcheck.py
+++ test/test_docmanager_delcheck.py
@@ -14,6 +14,7 @@ def test_docmanager_delcheck(tmp_valid_xml):
     handler = XmlHandler(tmp_valid_xml.strpath)
     handler.set({ustr: 'blub'})
     handler.delete(ustr)
+    handler.write()
 
     with open(tmp_valid_xml.strpath, 'r') as f:
         content = f.read()
@@ -26,3 +27,15 @@ def test_docmanager_delcheck2(tmp_valid_xml):
     dm = handler.tree.find("//dm:docmanager", namespaces=NS)
     dmset = set(localname(e.tag) for e in list(dm.iterchildren()) )
     assert dmset == set(DefaultDocManagerProperties)
+
+def test_docmanager_delcheck3(tmp_valid_xml):
+    ustr = 'ArnLTVMp'
+    handler = XmlHandler(tmp_valid_xml.strpath)
+    handler.set({'a/test/with/more/props/{}'.format(ustr): 'blub'})
+    handler.delete('a/test/with/more/props/{}'.format(ustr))
+    handler.write()
+
+    with open(tmp_valid_xml.strpath, 'r') as f:
+        content = f.read()
+    
+    assert ustr not in content, 'It seems that the content could not be deleted. Something seems to be wrong in the delete method of the XmlHandler.'
