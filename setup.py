6423f6f9bd909e080923a51d0d87054e38c6c0cc
Thomas Schraitle <tom_schr@web.de>
2015-08-06 15:18:52 +0200

    Restructured code into src

--- setup.py
+++ setup.py
@@ -93,7 +93,8 @@ setupdict = dict(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs']),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
