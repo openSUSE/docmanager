b6e3e8676e63b7deb3e82d01abcf47fb0f68c86c
Manuel Schnitzer <mschnitzer@suse.de>
2015-08-10 15:06:15 +0200

    Fix version test case

--- test/test_docmanager_version.py
+++ test/test_docmanager_version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import pytest
+import sys
 from docmanager import __version__
 from docmanager.cli import parsecli
 
@@ -18,6 +19,11 @@ def test_version_from_cli(capsys):
         pass
 
     out, err = capsys.readouterr()
-
-    assert out.split()[-1] == __version__
+    
+    # for some reason, the version number string from argparse
+    # will be printed to stderr and not to stdout
+    if (sys.version_info.major, sys.version_info.minor) != (3, 3):
+        assert out.split()[-1] == __version__
+    else:
+        assert err.split()[-1] == __version__
 
