07396a251256f8135ab44d2d6aec97869731dc28
Thomas Schraitle <tom_schr@web.de>
2017-01-04 16:58:38 +0100

    Replace PyTest with pytest-runner
    Taken from
    http://docs.pytest.org/en/latest/goodpractices.html#integrating-with-setuptools-python-setup-py-test-pytest-runner

--- setup.py
+++ setup.py
@@ -20,23 +20,6 @@ from setuptools.command.test import test as TestCommand
 
 here = path.abspath(path.dirname(__file__))
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 setupdict = dict(
     name='docmanager',
@@ -134,10 +117,9 @@ setupdict = dict(
     },
 
     # Required packages for testing
-    tests_require=['pytest'],
-
-    #
-    cmdclass = {'test': PyTest},
+    setup_requires=['pytest-runner',],
+    tests_require=['pytest', 'pytest-cov', ],
+    # 
 )
 
 # Call it:
