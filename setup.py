10f21aff411fdd83ac3bc4cc6cd195f94acc2d34
Thomas Schraitle <tom_schr@web.de>
2017-01-04 17:09:47 +0100

    Enable Python 3.4, 3.5, and 3.6, disable 3.3

--- setup.py
+++ setup.py
@@ -60,8 +60,9 @@ setupdict = dict(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
 
         # Supported Python versions
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
